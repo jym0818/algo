0ead76b7dcbdcb5ad0c3b1bef1839f128ce0105f
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-05-04 10:17:09 +0800

    单向链表

--- 02singlylinkedlist/main.go
+++ 02singlylinkedlist/main.go
@@ -14,31 +14,23 @@ type LinkedList struct {
 }
 
 func NewLinkedList() *LinkedList {
-	return &LinkedList{header: nil}
-}
-
-// 判断链表是否为空
-func (l *LinkedList) Isempty() bool {
-	return l.header == nil
+	return &LinkedList{header: &Node{Val: -1, Next: nil}}
 }
 
 // 打印链表
 func (l *LinkedList) Print() {
-	if l.Isempty() {
-		fmt.Println("空链表")
-		return
-	}
-	current := l.header
+	current := l.header.Next
 	for current != nil {
 		fmt.Printf("%d->", current.Val)
 		current = current.Next
 	}
+	fmt.Println("nil")
 }
 
 // 返回链表长度
 func (l *LinkedList) Length() int {
 	length := 0
-	current := l.header
+	current := l.header.Next
 	for current != nil {
 		length++
 		current = current.Next
@@ -46,45 +38,14 @@ func (l *LinkedList) Length() int {
 	return length
 }
 
-// 在链表头插入元素
-func (l *LinkedList) AddFromHead(val int) {
-	if l.Isempty() {
-		l.header = &Node{Val: val}
-		return
-	}
-	node := &Node{Val: val, Next: l.header}
-	l.header = node
-}
-
-// 在链表尾部插入元素
-func (l *LinkedList) AddFromTail(val int) {
-	node := &Node{
-		Val: val,
-	}
-	if l.Isempty() {
-		l.header = node
-		return
-	}
-	current := l.header
-	for current.Next != nil {
-		current = current.Next
-	}
-	current.Next = node
-}
-
 // 在任意位置i处插入  i从0开始
-func (l *LinkedList) Insert(val int, i int) {
-	if i <= 0 {
-		l.AddFromHead(val)
-		return
-	}
-	if i >= l.Length() {
-		l.AddFromTail(val)
-		return
+func (l *LinkedList) Insert(val int, index int) {
+	if index < 0 || index > l.Length() {
+		fmt.Println("边界错误")
 	}
 	//找到要插入位置i的前一个节点
 	preNode := l.header
-	for i := 0; i < i-1; i++ {
+	for i := 0; i < index; i++ {
 		preNode = preNode.Next
 	}
 	node := &Node{
@@ -95,48 +56,13 @@ func (l *LinkedList) Insert(val int, i int) {
 
 }
 
-//删除开头结点
-
-func (l *LinkedList) RemoveOfHead() {
-	if l.Isempty() {
-		fmt.Println("链表为空")
-		return
-	}
-	current := l.header
-	l.header = current.Next
-
-}
-
-//删除结尾结点
-
-func (l *LinkedList) RemoveOfTail() {
-	if l.Isempty() {
-		fmt.Println("链表为空")
-		return
-	}
-	current := l.header
-	if current.Next == nil {
-		l.header = nil
-		return
-	}
-	for current.Next.Next != nil {
-		current = current.Next
-	}
-	current.Next = nil
-}
-
 // 删除任意位置i的结点
 func (l *LinkedList) Remove(index int) {
-	if index <= 0 {
-		l.RemoveOfHead()
-		return
-	}
-	if index >= l.Length()-1 {
-		l.RemoveOfTail()
-		return
+	if index < 0 || l.header.Next == nil || index > l.Length()-1 {
+		return // 处理无效索引和空链表
 	}
 	preNode := l.header
-	for i := 0; i < index-1; i++ {
+	for i := 0; i < index; i++ {
 		preNode = preNode.Next
 	}
 	preNode.Next = preNode.Next.Next
@@ -145,31 +71,22 @@ func (l *LinkedList) Remove(index int) {
 //查找结点中是否包含某值
 
 func (l *LinkedList) Find(val int) bool {
-	if l.Isempty() {
-		fmt.Println("链表为空")
-		return false
-	}
-	current := l.header
-	flag := false
+
+	current := l.header.Next
+
 	for current != nil {
 		if current.Val == val {
-			flag = true
-			break
+			return true
 		}
 		current = current.Next
 	}
-	return flag
+	return false
 
 }
 
 func main() {
 	l := NewLinkedList()
-	l.AddFromTail(1)
-	l.AddFromTail(2)
-	l.AddFromTail(3)
-	l.AddFromTail(4)
 	l.Print()
 	fmt.Println()
-	l.RemoveOfTail()
 	l.Print()
 }
