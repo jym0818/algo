2ce833a21c06e93713b397cf95a45195d32b1497
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-05-04 11:13:23 +0800

    环形链表

--- 03LoopLinkedList/main.go
+++ 03LoopLinkedList/main.go
@@ -12,204 +12,83 @@ type LoopLinkedList struct {
 }
 
 func NewLoopLinkedList() *LoopLinkedList {
-	return &LoopLinkedList{
-		header: nil,
-	}
+	header := &Node{Val: 0}
+	header.Next = header // 关键闭环操作
+	return &LoopLinkedList{header: header}
 }
 
-// 判断环形链表是否为空
-func (l *LoopLinkedList) IsEmpty() bool {
-	return l.header == nil
-}
-
-//链表长度
-
+// 链表长度
 func (l *LoopLinkedList) Length() int {
-	if l.IsEmpty() {
-		return 0
-	}
-	length := 1
-	current := l.header
-	for current.Next != l.header {
-		current = current.Next
+	length := 0
+	current := l.header.Next
+	for current != l.header {
 		length++
+		current = current.Next
 	}
 	return length
-
-}
-
-// 头部插入元素
-func (l *LoopLinkedList) AddFromHead(val int) {
-	node := &Node{
-		Val: val,
-	}
-	if l.IsEmpty() {
-		l.header = node
-		node.Next = l.header
-		return
-	}
-	//找到最后一个node，让他指向新的元素
-	current := l.header
-	for current.Next != l.header {
-		current = current.Next
-	}
-	current.Next = node
-	node.Next = l.header
-	l.header = node
-
-}
-
-//尾部插入元素
-
-func (l *LoopLinkedList) AddFromTail(val int) {
-	node := &Node{Val: val}
-	if l.IsEmpty() {
-		l.header = node
-		node.Next = l.header
-		return
-	}
-
-	//找到尾结点
-	current := l.header
-	for current.Next != l.header {
-		current = current.Next
-	}
-	current.Next = node
-	node.Next = l.header
 
 }
 
 //任意位置插入元素 index从 0开始
 
 func (l *LoopLinkedList) Insert(val int, index int) {
-	if index <= 0 {
-		l.AddFromHead(val)
+	if index < 0 || index > l.Length() {
 		return
 	}
-	if index >= l.Length() {
-		l.AddFromTail(val)
-		return
-	}
+
 	current := l.header
-	for i := 0; i < index-1; i++ {
+	for i := 0; i < index; i++ {
 		current = current.Next
 	}
-	node := &Node{Val: val}
-	node.Next = current.Next
+	node := &Node{Val: val, Next: current.Next}
 	current.Next = node
-
-}
-
-// 删除头结点
-func (l *LoopLinkedList) ReomveOfHead() {
-	if l.IsEmpty() {
-		fmt.Println("链表为空")
-		return
-	}
-	//让头节点指向下个结点并且让最后一个结点也指向下个结点
-	current := l.header
-	//只有1个结点
-	if current.Next == l.header {
-		l.header = nil
-		return
-	}
-	for current.Next != l.header {
-		current = current.Next
-	}
-
-	current.Next = l.header.Next
-	l.header = l.header.Next
-
-}
-
-// 删除尾部元素
-func (l *LoopLinkedList) RemoveOfTail() {
-	if l.IsEmpty() {
-		fmt.Println("链表为空")
-		return
-	}
-
-	//找到倒数第二个结点 ，让它指向头节点
-	current := l.header
-	//如果链表只有1个元素
-	if current.Next == l.header {
-		l.header = nil
-		return
-	}
-	for current.Next.Next != l.header {
-		current = current.Next
-	}
-	current.Next = l.header
 }
 
 // 删除任意位置元素
 func (l *LoopLinkedList) Remove(index int) {
-	if l.IsEmpty() {
-		fmt.Println("Loop Linked List is Empty")
-		return
+	if index < 0 || index >= l.Length() || l.Length() == 0 {
+		return // 索引越界直接返回
 	}
-	if index <= 0 {
-		l.ReomveOfHead()
-		return
-	}
-	if index >= l.Length()-1 {
-		l.RemoveOfTail()
-		return
-	}
+
 	current := l.header
-	for i := 0; i < index-1; i++ {
+	for i := 0; i < index; i++ {
 		current = current.Next
 	}
-	current.Next = current.Next.Next
+	current.Next = current.Next.Next // 自动保持闭环
 
 }
 
 //查找元素
 
 func (l *LoopLinkedList) Find(val int) bool {
-	if l.IsEmpty() {
-		return false
-	}
-	current := l.header
-	flag := false
-
-	for current.Next != l.header {
+	current := l.header.Next // 从第一个实际节点开始
+	for current != l.header {
 		if current.Val == val {
-			flag = true
-			break
+			return true
 		}
 		current = current.Next
 	}
-	if current.Val == val {
-		flag = true
-	}
-	return flag
+	return false
 }
 
 //遍历链表
 
 func (l *LoopLinkedList) Print() {
-	if l.IsEmpty() {
-		fmt.Println("链表为空")
+	if l.header.Next == l.header {
+		fmt.Println("空链表")
 		return
 	}
-	current := l.header
 
-	for current.Next != l.header {
+	current := l.header.Next
+	for current != l.header {
 		fmt.Printf("%d->", current.Val)
 		current = current.Next
 	}
-	fmt.Printf("%d->", current.Val)
-
+	fmt.Println("头哨兵")
 }
 
 func main() {
 	l := NewLoopLinkedList()
-	l.Insert(1, 0)
-	l.Insert(2, -1)
-	l.Insert(3, 4)
-	l.Insert(4, 6)
-	l.Insert(10, 2)
 
 	l.Print()
 
